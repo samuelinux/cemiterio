66c8cfbc22c998c5454e9e1b44d79641d31ae934
samuelinux <samuelpereiramachado@gmail.com>
2025-09-19 16:45:20 -0300

    Feat: Adicionado visualizar certidão de obito na listagem

--- resources/views/livewire/empresa/sepultamento/index.blade.php
+++ resources/views/livewire/empresa/sepultamento/index.blade.php
@@ -83,6 +83,19 @@
                         </td>
                         <td class="px-3 py-2 text-right">
                             <div class="inline-flex items-center gap-2">
+                                @if ($s->certidao_obito_path)
+                                    <a href="{{ Storage::url($s->certidao_obito_path) }}" target="_blank"
+                                        class="px-3 py-1.5 bg-green-600 text-white rounded-md text-sm hover:bg-green-700 transition">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                            class="size-6">
+                                            <path
+                                                d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z" />
+                                            <path
+                                                d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
+                                        </svg>
+
+                                    </a>
+                                @endif
                                 @if ($canEdit)
                                     <button type="button" @click="showEdit = true; $wire.edit({{ $s->id }})"
                                         class="px-3 py-1.5 bg-blue-600 text-white rounded-md text-sm hover:bg-blue-700 transition">
@@ -95,6 +108,7 @@
                                         Excluir
                                     </button>
                                 @endif
+
                             </div>
                         </td>
                     </tr>
@@ -122,6 +136,19 @@
                 </div>
 
                 <div class="mt-3 flex justify-end gap-2">
+                    @if ($s->certidao_obito_path)
+                        <a href="{{ Storage::url($s->certidao_obito_path) }}" target="_blank"
+                            class="px-3 py-1.5 bg-green-600 text-white rounded-md text-sm hover:bg-green-700 transition">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                class="size-6">
+                                <path
+                                    d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625Z" />
+                                <path
+                                    d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
+                            </svg>
+
+                        </a>
+                    @endif
                     @if ($canEdit)
                         <button type="button" @click="showEdit = true; $wire.edit({{ $s->id }})"
                             class="px-3 py-1.5 bg-blue-600 text-white rounded-md text-sm hover:bg-blue-700 transition">
